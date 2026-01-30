60ed5fa0069ddd7d06ac63a712ac18ecc18e05b9
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-01-30 19:39:50 +0100

    Add shared QT core wrapper

---
+++ AIOneQTWidgets/AIOneQTCore/QLLModel.hpp
@@ -0,0 +1,7 @@
+
+#include <LLModel.hpp>
+#include <QObject>
+
+class QLLModel : public LLModel, public QObject {
+
+};
--- AIOneQTWidgets/mainwindow.cpp
+++ AIOneQTWidgets/mainwindow.cpp
@@ -8,24 +8,24 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->listWidget->addItem("hey");
-    ui->listWidget->addItem("hey");
 
     connect(ui->sendButton, &QPushButton::clicked, [this]() {
         qDebug() << "I felt that!";
 
         QString message = ui->messageInput->toPlainText();
+        ui->messageInput->setPlainText("");
 
-        ui->listWidget->addItem("Response: ");
+        ui->listWidget->addItem(message);
 
         llm->generateAsync(message.toStdString(), [this](std::string token) {
             QString response(token.c_str());
             qDebug() << response;
             // ui->listWidget->addItem(response);
-            QString newText = ui->listWidget->currentItem()->text() + response;
+            // QString newText = ui->listWidget->currentItem()->text() + response;
             auto lastItemIndex = ui->listWidget->count() - 1;
-            // auto lastItem =
-                ui->listWidget->item(lastItemIndex)->setText(newText);
+            auto lastItem = ui->listWidget->item(lastItemIndex);
+            QString newText = lastItem->text() + response;
+            lastItem->setText(newText);
         });
     });
 
