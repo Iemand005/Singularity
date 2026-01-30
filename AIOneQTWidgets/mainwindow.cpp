f176d95909bf857cadca6dcec92ce1ec9f00f501
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-01-30 17:02:27 +0100

    wt widgets filename and stuff

--- AIOneQTWidgets/mainwindow.cpp
+++ AIOneQTWidgets/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -8,6 +10,23 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->listWidget->addItem("hey");
     ui->listWidget->addItem("hey");
+
+    connect(ui->sendButton, &QPushButton::clicked, [this]() {
+        qDebug() << "I felt that!";
+
+    });
+
+    connect(ui->loadLLMButton, &QPushButton::clicked, [this]() {
+        qDebug() << "I need that!";
+
+        QString fileName = QFileDialog::getOpenFileName(
+            this,                    // Parent widget
+            tr("Open GGUF file"),         // Dialog title
+            QDir::homePath(),        // Starting directory
+            tr("GGUF files (*.gguf);")
+            );
+        qDebug() << "and this is the file" << fileName;
+    });
 }
 
 MainWindow::~MainWindow()
