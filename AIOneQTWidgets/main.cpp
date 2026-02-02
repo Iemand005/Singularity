a05d6354bb6a51849d7c1964017b4f39e9588e10
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-02 12:59:18 +0100

    Remove CRT debug

--- AIOneQTWidgets/main.cpp
+++ AIOneQTWidgets/main.cpp
@@ -4,8 +4,6 @@
 #include <QLocale>
 #include <QTranslator>
 
-#include <crtdbg.h>
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -20,7 +18,6 @@ int main(int argc, char *argv[])
         }
     }
     MainWindow w;
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     w.show();
     return a.exec();
 }
