e01ffbeac59839acedba6d24a8b85464e913cd81
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-04 22:54:35 +0100

    Remove old code

--- UIHandler.cpp
+++ UIHandler.cpp
@@ -35,49 +35,6 @@ void UIHandler::prompt(const QString &message) {
         tokenReceived(token);
     };
     chatManager->sendAsync(message, options); // This does what all that garble below used todo
-    // Message chatMessage = {"user", message.toStdString()};
-    // chatMessages->push_back(chatMessage);
-
-    // std::string finalPrompt = this->llm->chatToPrompt(*chatMessages);
-    // qDebug() << "Generating response to:" << finalPrompt;
-
-    // if (workerThread && workerThread->isRunning()) {
-    //     qWarning() << "Already processing a prompt";
-    //     return;
-    // }
-
-    // workerThread = new QThread();
-
-    // QObject::connect(workerThread, &QThread::started, [this, finalPrompt]() {
-    //     QMutexLocker locker(&llmMutex);
-
-    //     if (!llm) {
-    //         qWarning() << "LLM model not loaded";
-    //         return;
-    //     }
-
-    //     TextGenOptions options;
-    //     options.onToken = [this](const std::string token)
-    //     {
-    //         tokenReceived(token.c_str());
-    //     };
-
-    //     TextGenResult stats = this->llm->completeAny(finalPrompt, options);
-
-    //     std::cout << "Finished generation" << std::endl;
-    //     std::cout << "- " << std::to_string(stats.tokensGenerated) << " tokens generated" << std::endl;
-    //     std::cout << std::endl;
-
-    //     chatMessages->push_back(stats.output);
-
-    //     workerThread->quit();
-    // });
-    // QObject::connect(workerThread, &QThread::finished, [this]() {
-    //     workerThread->deleteLater();
-    //     workerThread = nullptr;
-    // });
-
-    // workerThread->start();
 }
 
 void UIHandler::loadSDModel(const QString &path) {
