65cfdafb4b0e84d91846791c779a6c5855ad403c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-29 04:56:35 +0200

    bruh colon my ine

--- AIOneQTWidgets/mainwindow.cpp
+++ AIOneQTWidgets/mainwindow.cpp
@@ -197,7 +197,7 @@ MainWindow::MainWindow(QWidget *parent)
     // Uh OpenAI API stuff
 
     connect(ui->openAIButton, &QPushButton::clicked, [&]() {
-      std::string uhUrl = "";
+      std::string uhUrl = "https://api.groq.com/openai/v1";
       std::string apiKey = ui->openAIKey->text().toStdString();
       static OpenAIClient client(uhUrl, apiKey);
 
