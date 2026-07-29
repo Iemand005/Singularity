61de0532a326152c202ffca49076535f6d202a46
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-29 22:47:54 +0200

    hmm no thing no path in the url

--- AIOneQTWidgets/mainwindow.cpp
+++ AIOneQTWidgets/mainwindow.cpp
@@ -197,7 +197,7 @@ MainWindow::MainWindow(QWidget *parent)
     // Uh OpenAI API stuff
 
     connect(ui->openAIButton, &QPushButton::clicked, [&]() {
-      std::string uhUrl = "https://api.groq.com/openai";
+      std::string uhUrl = "api.groq.com/openai";
       std::string apiKey = ui->openAIKey->text().toStdString();
       static OpenAIClient client(uhUrl, apiKey);
 
